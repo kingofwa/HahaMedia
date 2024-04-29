63ac938f1e7ba0897a8c0cee8a15506f5237f8db
LeNguyenVan <Lenguyenvan09@gmail.com>
2024-04-29 23:20:46 +0700

    Add mapper profile

--- HahaMedia/Src/Core/HahaMedia.Application/Features/Songs/Commands/CreateSong/CreateSongHandler.cs
+++ HahaMedia/Src/Core/HahaMedia.Application/Features/Songs/Commands/CreateSong/CreateSongHandler.cs
@@ -10,19 +10,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using HahaMedia.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using HahaMedia.Application.Mappings;
+using AutoMapper;
 
 namespace HahaMedia.Application.Features.Songs.Commands.CreateSong
 {
-    public class CreateSongHandler(ISongRepository songRepository, IUnitOfWork unitOfWork) : IRequestHandler<CreateSongCommand, BaseResult<Guid>>
+    public class CreateSongCommandHandler : IRequestHandler<CreateSongCommand, BaseResult<Guid>>
     {
+        private readonly IMapper _mapper;
+        private readonly ISongRepository _songRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateSongCommandHandler(IMapper mapper, ISongRepository songRepository, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _songRepository = songRepository;
+            _unitOfWork = unitOfWork;
+        }
 
         public async Task<BaseResult<Guid>> Handle(CreateSongCommand request, CancellationToken cancellationToken)
         {
+            var song = _mapper.Map<CreateSongCommand, Song>(request);
 
-            var song = new Song();
-
-            await songRepository.AddAsync(song);
-            await unitOfWork.SaveChangesAsync();
+            await _songRepository.AddAsync(song);
+            await _unitOfWork.SaveChangesAsync();
 
             return new BaseResult<Guid>(song.Id);
         }
